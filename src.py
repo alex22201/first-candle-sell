92cb425f999ca6947277adbd832af90359ad7238
alex22201 <tyutyunnikaliosha@gmail.com>
2024-08-26 22:10:33 +0300

    Fix exchange initialization and error handling

--- src.py
+++ src.py
@@ -9,26 +9,31 @@ from utils import logger
 async def place_order(exchange: Exchange, symbol: str, amount: float, price: float) -> None:
     try:
         order = exchange.create_limit_sell_order(symbol, amount, price)
-        print(f'Order placed: {order}')
+        logger.info(f'Order placed: {order}')
     except ccxt.NetworkError as e:
         logger.error(f'Network error while placing order: {e}')
-    except ccxt.ExchangeError as e:
+    except (ccxt.BadRequest, ccxt.OperationRejected) as e:
         logger.error(f'Exchange error while placing order: {e}')
-    except Exception as e:
-        logger.error(f'Unexpected error: {e}')
+    except (ccxt.AuthenticationError, ccxt.InsufficientFunds) as e:
+        logger.error(f'Error: {e}')
+        exit()
 
 
 async def main(exchange: Exchange, symbol: str, amount: float, price: float) -> None:
     while True:
         await place_order(exchange, symbol, amount, price)
-        await asyncio.sleep(0.01)  # set delay
+        # await asyncio.sleep(0.01)  # set delay
 
 
 def initialize_exchange(exchange_name: str, api_key: str, api_secret: str, api_password: str = None) -> Exchange:
-    exchange_class = getattr(ccxt, exchange_name)
-    exchange = exchange_class({
-        'apiKey': api_key,
-        'secret': api_secret,
-        'password': api_password
-    })
-    return exchange
+    try:
+        exchange_class = getattr(ccxt, exchange_name)
+        exchange = exchange_class({
+            'apiKey': api_key,
+            'secret': api_secret,
+            'password': api_password
+        })
+        return exchange
+    except AttributeError as e:
+        logger.error(f'Unsupported exchange {exchange_name}: {e}')
+        exit()
--- utils.py
+++ utils.py
@@ -5,14 +5,14 @@ import logging
 # Load environment variables from .env file
 load_dotenv()
 
-exchange_name = os.getenv('EXCHANGE_NAME')
+exchange_name = os.getenv('EXCHANGE_NAME').lower()
 api_key = os.getenv('API_KEY')
 api_secret = os.getenv('SECRET_KEY')
 api_password = os.getenv('PASSWORD', '')
 
 symbol = os.getenv('PAIR_SYMBOL', '')
-amount = os.getenv('AMOUNT', '')
-price = os.getenv('PRICE', '')
+amount = float(os.getenv('AMOUNT', ''))
+price = float(os.getenv('PRICE', ''))
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
